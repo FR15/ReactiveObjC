ab7c192d2d1409869d1e2f62f97befc27fa53a53
sun.yunxin <sun.yunxin@xiaoyi.com>
2019-06-24 19:58:58 +0800

    ai

--- ReactiveObjC/Stream/RACDynamicSignal.h
+++ ReactiveObjC/Stream/RACDynamicSignal.h
@@ -8,6 +8,15 @@
 
 #import "RACSignal.h"
 
+// everything is stream
+
+// A 是一个事件源
+// 随着时间不断产生事件
+// 事件沿着管道流动
+//
+// 这个管道可以接入任意的分支，将事件截流
+// 接入的方式的就是 订阅
+
 // A private `RACSignal` subclasses that implements its subscription behavior
 // using a block.
 @interface RACDynamicSignal : RACSignal
